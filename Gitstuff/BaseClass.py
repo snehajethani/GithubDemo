5cd93ccb077dd4405bbc1350ac18b5ec06b77530
snehajethani <snehajethanijdcoem@gmail.com>
2022-09-01 01:34:10 +0530

    new branch development

--- Gitstuff/BaseClass.py
+++ Gitstuff/BaseClass.py
@@ -14,3 +14,17 @@ class BaseClass:
 
         logger.setLevel(logging.DEBUG)
         return logger
+
+    def test_loggingDemo2(self):
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
+        filehandler = logging.FileHandler("logfile.log")
+        formater = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s : %(message)s")
+        filehandler.setFormatter(formater)
+
+        logger.addHandler(filehandler)
+
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+
