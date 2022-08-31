a4748210528c59a418eeecff0f3b522d070372f6
snehajethani <snehajethanijdcoem@gmail.com>
2022-09-01 01:21:14 +0530

    Third commit from abc

--- Gitstuff/childwindow.py
+++ Gitstuff/childwindow.py
@@ -15,3 +15,19 @@ print(driver.find_element(By.TAG_NAME,"h3").text)
 
 driver.switch_to.window(windows_opened[0])
 print(driver.find_element(By.TAG_NAME,"h3").text)
+
+driver.switch_to.window(windows_opened[0])
+print(driver.find_element(By.TAG_NAME,"h3").text)
+
+
+driver.get("https:/the-internet.herokuapp.com/windows")
+driver.find_element(By.LINK_TEXT,"Click Here").click()
+windows_opened = driver.window_handles
+driver.switch_to.window(windows_opened[1])
+print(driver.find_element(By.TAG_NAME,"h3").text)
+
+driver.switch_to.window(windows_opened[0])
+print(driver.find_element(By.TAG_NAME,"h3").text)
+
+driver.switch_to.window(windows_opened[0])
+print(driver.find_element(By.TAG_NAME,"h3").text)
